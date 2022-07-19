a10f0a25a5c9eec6dd2666aeca8ff9b61bc649f8
tanmay107 <wills4986@gmail.com>
2022-07-19 15:12:51 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+const firebase = require('firebase');
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_PUBLIC_KEY,
+    authDomain: "fir-auth-ecommerce-9b085.firebaseapp.com",
+    projectId: "fir-auth-ecommerce-9b085",
+    storageBucket: "fir-auth-ecommerce-9b085.appspot.com",
+    messagingSenderId: "282818523080",
+    appId: "1:282818523080:web:5a80205ec1b60759b7dafd",
+    measurementId: "G-9ZBZWHQ0YD"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const products = db.collection("products");
+const orders = db.collection("orders");
+module.exports = { products: products, orders: orders };
---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+const cors = require('cors');
+const db = require('./config');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/", async (req, res) => {
+    const snapshot = await db.products.get();
+    const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    res.json(list);
+});
+
+app.post("/create", async (req, res) => {
+    const data = req.body;
+    await db.products.add({ data });
+    res.send({ msg: "product Added" });
+  });
+
+app.get("/orders", async (req, res) => {
+    const snapshot = await db.orders.get();
+    const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    res.send(list);
+})
+
+
+
+app.listen(4000, () => console.log("Up & Running *4000"));
