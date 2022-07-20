b0d98caeac36e1a0448ec9b3d71d9fab63ff3ed7
tanmay107 <wills4986@gmail.com>
2022-07-20 13:21:42 +0530

    added getting orders list and products list and creations

--- index.js
+++ index.js
@@ -5,31 +5,19 @@ const db = require('./config');
 const app = express();
 app.use(express.json());
 app.use(cors());
+const PORT = process.env.PORT || 4000
 
-
-app.get("/", async (req, res) => {
+app.get("/api", async (req, res) => {
     const snapshot = await db.products.get();
     const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
     res.json(list);
 });
 
-// app.post("/create", async (req, res) => {
-//     const data = req.body;
-//     await db.products.add({ data });
-//     res.send({ msg: "product Added" });
-//   });
-
-// app.get("/orders", async (req, res) => {
-//     const snapshot = await db.orders.get();
-//     const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-//     res.send(list);
-// })
-
 const productRouter = require('./routes/products');
-app.use("/products", productRouter);
+app.use("/api/products", productRouter);
 
 const orderRouter = require('./routes/orders');
-app.use("/order", orderRouter);
+app.use("/api/orders", orderRouter);
 
 
-app.listen(4000, () => console.log("Up & Running *4000"));
+app.listen(PORT, () => console.log("Up & Running *4000"));
--- routes/products.js
+++ routes/products.js
@@ -1,17 +1,30 @@
 const express = require('express');
-const db = require('../config');
+const { db } = require('../config/firebase-config');
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    const snapshot = await db.products.get();
-    const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    res.json(list);
+    const productRef = await db.collection("products");
+    try{
+            productRef.get().then((snapshot) => {
+            const data = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
+            console.log(data);
+            return res.status(201).json(data);
+        })
+    } catch (e) {
+        return res.status(500).json({ general: "Something went wrong, please try again"});
+    }
 });
 
 router.post("/create", async (req, res) => {
-    const data = req.body;
-    await db.products.add({ data });
-    res.send({ msg: "product Added" });
+    try{
+        const productsRes = await db.collection('products').add(req.body)
+        return res.status(201).json({ "message": "Added" })
+    } catch(e) {
+        return res.status(500).json({ general: "Something went wrong, please try again"});
+    }  
 })
 
 module.exports = router;
