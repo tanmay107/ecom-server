8f76176194db181ad6b17c22a16a73f73f047ff0
tanmay107 <wills4986@gmail.com>
2022-07-20 15:49:06 +0530

    changed admin.auth() to getAuth() in middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,10 +1,11 @@
 const admin = require('../config/firebase-config');
+const { getAuth } = require('firebase-admin/auth');
 
 class Middleware {
 	async decodeToken(req, res, next) {
 		const token = req.headers.authorization.split(' ')[1];
 		try {
-			const decodeValue = await admin.auth().verifyIdToken(token);
+			const decodeValue = await getAuth().verifyIdToken(token);
 			console.log(decodeValue);
 			if (decodeValue) {
 				req.user = decodeValue;
