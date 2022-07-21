0e0ee1ecf4ffab22ae50e3535611ea82c7385a33
tanmay107 <wills4986@gmail.com>
2022-07-21 16:46:56 +0530

    Debug

--- routes/carts.js
+++ routes/carts.js
@@ -21,7 +21,7 @@ router.post('/', async (req, res) => {
 router.post("/create", async (req, res) => {
     try{
         const cartsRes = await db.collection('carts').add(req.body)
-        return res.status(201).json({ "message": "Added" })
+        return res.status(201).json({ message: "Added" })
     } catch(e) {
         return res.status(500).json({ general: "Something went wrong, please try again"});
     }  
@@ -31,7 +31,8 @@ router.post("/update", async (req, res) => {
     try {
         console.log(req.header('Id'))
         const cartsRes = await db.collection('carts').doc(req.header('Id')).set(req.body, { merge: true });
-        return res.status(201).json({ "message": "Upgraded" })
+        console.log("Carts res info ----------------------- ", cartsRes);
+        return res.status(201).json({ message: "Updated" })
     } catch (e) {
         return res.status(500).send(e);
     }
@@ -40,7 +41,7 @@ router.post("/update", async (req, res) => {
 router.post("/delete", async (req,res) => {
     try{
         const cartsRes = await db.collection('carts').doc(req.body.docid).delete();
-        return res.status(201).json({ "message" : "Deleted" })
+        return res.status(201).json({ message : "Deleted" })
     } catch(e) {
         return res.status(500).json({ general: "Something went wrong, please try again"});
     }
